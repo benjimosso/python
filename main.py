cc0ff3791d95d4ca676338cfd4829b685d4c7a1b
benjimosso <benjimosso@gmail.com>
2024-06-04 16:56:33 -0700

    second commit

--- main.py
+++ main.py
@@ -17,10 +17,30 @@ def deposits():
 
 
 def get_number_of_lines():
+     while True:
+        lines = input("Enter the numbers of lines to bet on (1-" + str(MAX_LINES) + "): ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines")
+        else:
+            print("Please enter a number")
+        
+        return lines
+   
+
+
+    
+  
+
+  
 
 
 def main():
     balance = deposits()
-    pass
+    lines = get_number_of_lines()
+    print(balance, lines)
    
 main()    
